9f7305838cf3029f6aa48debb26787b92b4551f4
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-10-24 10:34:35 +0300

    feat: add header select styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,12 +8,24 @@ import { GlobalSvgSelector } from 'assets/images/icons/global/globalSvgSelector'
 import { ReturnComponentType } from 'types';
 
 const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
+  { value: 'city-1', label: 'Санкт-Петербург' },
+  { value: 'city-2', label: 'Москва' },
+  { value: 'city-3', label: 'Новгород' },
 ];
 
 export const Header = (): ReturnComponentType => {
+  const colorStyles = {
+    control: (styles: any) => ({
+      ...styles,
+      backgroundColor: 'rgba(71,147,255,0.2)',
+      width: '194px',
+      height: '37px',
+      border: 'none',
+      borderRadius: '10px',
+      zIndex: '10',
+    }),
+  };
+
   return (
     <header className={s.header}>
       <div className={s.wrapper}>
@@ -26,7 +38,7 @@ export const Header = (): ReturnComponentType => {
         <div className={s.change_theme}>
           <GlobalSvgSelector id="change-theme" />
         </div>
-        <Select options={options} />
+        <Select defaultValue={options[0]} options={options} styles={colorStyles} />
       </div>
     </header>
   );
